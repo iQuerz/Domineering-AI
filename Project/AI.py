e198445b10c62fd8bf0a41aa3a60e9776168ec02
iQuerz <niki.tenis@gmail.com>
2022-11-28 20:34:57 +0100

    Smol random AI baby method

--- Project/AI.py
+++ Project/AI.py
@@ -1,3 +1,8 @@
+import random
 
-def getNextMove(matrix):
-    return (0,0)
+def getNextMove(matrix, isMoveValid):
+    while True:
+        i=random.randrange(0, len(matrix))
+        j=random.randrange(0, len(matrix[0]))
+        if(isMoveValid(i, j, matrix)):
+            return (i, j)
