7dd2b2228b1e04e7e598a038f18f8a6b906a8371
iQuerz <niki.tenis@gmail.com>
2022-11-29 15:48:08 +0100

    Add matrix size and AI player options through args

--- Project/Board/const.py
+++ Project/Board/const.py
@@ -1,7 +1,17 @@
 from tkinter import *
+import sys
 
-COLS = 8
 ROWS = 8
+COLS = 8
+
+AI_TURN = 0 #0=pvp, 1=prvi igra AI, 2=drugi
+
+length = len(sys.argv)
+if length >= 3:
+    ROWS = int(sys.argv[1])
+    COLS = int(sys.argv[2])
+    if length >= 4:
+        AI_TURN = int(sys.argv[3])
 
 WIDTH = COLS * 80
 HEIGHT = ROWS * 80
